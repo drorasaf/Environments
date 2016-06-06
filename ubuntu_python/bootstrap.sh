15b2cda29e0a5516d76e02dd9639f57aa1c576b0
Dror <hellfliers@walla.co.il>
2016-06-06 03:22:37 +0300

    change script to download and install conda only if necessary

--- ubuntu_python/bootstrap.sh
+++ ubuntu_python/bootstrap.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 
 apt-get update
-apt-get install -y apache2
-if ! [ -L /var/www ]; then
-  rm -rf /var/www
-  ln -fs /vagrant /var/www
+if ! [ -d /home/vagrant/anaconda3 ]; then
+  wget http://repo.continuum.io/archive/Anaconda3-4.0.0-Linux-x86_64.sh
+  sudo chmod +x /home/vagrant/Anaconda3-4.0.0-Linux-x86_64.sh
+  sudo /home/vagrant/Anaconda3-4.0.0-Linux-x86_64.sh -b
 fi
+export PATH="/home/vagrant/anaconda3/bin":$PATH
+conda update -y conda
+conda update -y anaconda
